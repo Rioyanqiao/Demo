540805e040e23402646e676e284fc366bd93634a
Ayanscorpio <847386249@qq.com>
2018-10-12 11:05:32 +0800

    first commit

--- PyramidDemo/src/PyramidDemo/PyramidDemo.java
+++ PyramidDemo/src/PyramidDemo/PyramidDemo.java
@@ -5,8 +5,8 @@ import java.io.*;
 public class PyramidDemo {
 	public static void main(String[] args)   
 	{  
-	    Dayin dayin=new Dayin();  
-	    dayin.Dayin1();  
+	    Dayin1 dayin=new Dayin1();  
+	    dayin.Dayin:();  
 	}  
 
 }
@@ -43,4 +43,37 @@ public void Dayin1()
     }  
           
 }
+}
+class Dayin1
+{
+	public void Dayin()
+	{
+	    try
+	    {
+	        BufferedReader br;
+	        br = new BufferedReader(new InputStreamReader(System.in));
+	        System.out.println("请输入金字塔层数：");
+	        String ceng1=br.readLine();  //从控制台读取
+	        int ceng = Integer.parseInt(ceng1);  //把string->int
+
+	        for (int i=ceng-1;i>=0;i-- )
+	        {
+	        	for (int k=1;k<ceng-i ;k++ )  //空格规律
+	            {
+	                System.out.print(" ");  //打印空格
+	            }
+	        	for (int j=0; j<=i;j++ )
+	                {
+	                    System.out.print("*"+" ");  //打印星和空格
+	                }
+
+	            System.out.print("\n");
+	        }
+	    }
+	    catch (Exception e)
+	    {
+	        e.printStackTrace();
+	    }
+
+	}
 }
